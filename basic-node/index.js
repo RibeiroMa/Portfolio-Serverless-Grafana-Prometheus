53a3751a2fb5a2175369ebd438d41fc977b27473
RibeiroMa <matheus.ribe2018@gmail.com>
2024-08-14 19:46:13 -0300

    Serverless

--- basic-node/index.js
+++ basic-node/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+const helloWorld = () => ({ message: "Hello World!2" });
+
+const sum = (a, b) => ({ resultado: a + b });
+
+const helloWorldExpress = (req, res) => {
+  const resposta = helloWorld();
+  return res.json(resposta);
+};
+const sumExpress = (req, res) => {
+  const resposta = sum(+req.query.a, +req.query.b);
+  return res.json(resposta);
+};
+
+app.get("/sum", sumExpress);
+
+app.get("/", helloWorldExpress);
 
 const { PORT = 4000 } = process.env;
 app.listen(PORT, () => {
---
+++ basic-serverless-node/index.js
@@ -0,0 +1,65 @@
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+
+const helloWorld = () => ({ message: "Hello World!" });
+exports.helloWorld = async function (event, context) {
+  const resposta = helloWorld();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(resposta),
+  };
+};
+
+const sum = (a, b) => ({ resultado: a + b });
+exports.sum = async function (event, context) {
+  const resposta = sum(
+    +event.queryStringParameters.a,
+    +event.queryStringParameters.b
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify(resposta),
+    // body: JSON.stringify(event)
+  };
+};
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+function obterDataHoraAtual() {
+  // Cria um novo objeto Date com a data e hora atuais
+  const agora = new Date();
+
+  // Extrai o ano, mês, dia, hora, minuto e segundo
+  const ano = agora.getFullYear();
+  const mes = String(agora.getMonth() + 1).padStart(2, "0"); // Meses são baseados em zero
+  const dia = String(agora.getDate()).padStart(2, "0");
+  const hora = String(agora.getHours()).padStart(2, "0");
+  const minuto = String(agora.getMinutes()).padStart(2, "0");
+  const segundo = String(agora.getSeconds()).padStart(2, "0");
+
+  // Monta a string no formato desejado
+  return `${ano}-${mes}-${dia}-${hora}-${minuto}-${segundo}`;
+}
+
+const dynamo = async (valor) => {
+  const command = new PutItemCommand({
+    TableName: "AulaJp",
+    Item: {
+      id: {
+        S: obterDataHoraAtual(),
+      },
+      nome: {
+        S: valor,
+      },
+    },
+  });
+
+  await client.send(command);
+  return true;
+};
+exports.dynamo = async function (event, context) {
+  const resposta = await dynamo(event.pathParameters.valor);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(resposta),
+  };
+};
